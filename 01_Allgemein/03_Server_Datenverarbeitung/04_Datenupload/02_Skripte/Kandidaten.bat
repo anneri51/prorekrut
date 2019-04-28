71575f43723d82d4de5f8981dc41654c6bb6d192
ari <anneri51@gmx.de>
2019-04-28 19:06:13 +0200

    compr dateien

---
+++ 01_Allgemein/03_Server_Datenverarbeitung/04_Datenupload/02_Skripte/Kandidaten.bat
@@ -0,0 +1,3 @@
+d:
+cd D:\GitProjekte\02_proj2_prorekrut\git\01_Allgemein\03_Server_Datenverarbeitung\04_Datenupload\01_src_data\01_Kandidaten\01_Details
+for /r %%i in (*) do type %%i   >>../aa_appl_coord_all.txt
---
+++ 01_Allgemein/03_Server_Datenverarbeitung/04_Datenupload/02_Skripte/firmen.bat
@@ -0,0 +1,3 @@
+d:
+cd D:\GitProjekte\02_proj2_prorekrut\git\01_Allgemein\03_Server_Datenverarbeitung\04_Datenupload\01_src_data\02_Firmen\01_Details
+for /r %%i in (*) do type %%i   >>../aa_comp_coord_all.txt
